aee8589b01b46a0971dfc5cd9d91e6a9650ec8d8
Eduardo Trandafilov <eduardo.trandafilov@gmail.com>
2025-04-07 23:59:23 -0300

    ajuste identação

--- mod1-desafio/src/desafio/Principal.java
+++ mod1-desafio/src/desafio/Principal.java
@@ -2,35 +2,34 @@ package desafio;
 
 public class Principal {
 	public static void main(String[] args) {
-		
+
 		Pessoa pessoa = new Pessoa();
 		pessoa.exibeMsg();
-		
+
 		Calculadora calc = new Calculadora();
 		System.out.println(calc.retornaDobro(10));
-		
+
 		Musica musica = new Musica();
-		musica.titulo =  "Hip hop";
+		musica.titulo = "Hip hop";
 		musica.artista = "Joao";
 		musica.anoLancamento = 1990;
-		
+
 		musica.exibeFichaTecnica();
 		musica.avaliar(10);
 		musica.avaliar(20);
-		System.out.println("Media avaliacao" +musica.calcularMediaAvaliacao());
-			
+		System.out.println("Media avaliacao" + musica.calcularMediaAvaliacao());
+
 		Carro carro = new Carro();
 		carro.modelo = "ford";
 		carro.cor = "azul";
 		carro.ano = 1980;
-		
+
 		carro.exibirFichaTecnica();
-	    System.out.println(carro.calcularIdade());
-	    
-	    Aluno aluno = new Aluno();
-	    aluno.nome = "Eduardo";
-	    aluno.idade= 20;
-	    aluno.exibirInformações();
-	    
+		System.out.println(carro.calcularIdade());
+
+		Aluno aluno = new Aluno();
+		aluno.nome = "Eduardo";
+		aluno.idade = 20;
+		aluno.exibirInformações();
 	}
 }
